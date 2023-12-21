ca954a46536b21f57d3521d5eca20bc3fd3d25e2
Trevor <trevorhilz@gmail.com>
2023-12-20 19:18:46 -0600

    added many icons, edited the scripts, css, html. deleted bowser

---
+++ script.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // const newCursor = document.getElementById('newCursor');
+  const cookie = document.getElementById('button1');
+  cookie.addEventListener('click', () => {
+    console.log('click');
+    let cursor = '/img/cookie.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const hotdog = document.getElementById('button2');
+  hotdog.addEventListener('click', () => {
+    let cursor = '/img/hotdog.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const hamburger = document.getElementById('button3');
+  hamburger.addEventListener('click', () => {
+    let cursor = '/img/hamburger.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const banana = document.getElementById('button4');
+  banana.addEventListener('click', () => {
+    let cursor = '/img/banana.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const sus = document.getElementById('button5');
+  sus.addEventListener('click', () => {
+    let cursor = '/img/sus.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const snowman = document.getElementById('button6');
+  snowman.addEventListener('click', () => {
+    let cursor = '/img/snowman.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const gingerbread = document.getElementById('button7');
+  gingerbread.addEventListener('click', () => {
+    let cursor = '/img/ginger-bread.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const diamondSword = document.getElementById('button8');
+  diamondSword.addEventListener('click', () => {
+    let cursor = '/img/diamond-sword.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const blueArrow = document.getElementById('button9');
+  blueArrow.addEventListener('click', () => {
+    let cursor = '/img/blue-arrow.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const shovel = document.getElementById('button10');
+  shovel.addEventListener('click', () => {
+    let cursor = '/img/shovel.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const duck = document.getElementById('button11');
+  duck.addEventListener('click', () => {
+    let cursor = '/img/duck.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const nayn = document.getElementById('button12');
+  nayn.addEventListener('click', () => {
+    let cursor = '/img/nayn.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const waffle = document.getElementById('button13');
+  waffle.addEventListener('click', () => {
+    let cursor = '/img/waffle.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const coffee = document.getElementById('button14');
+  coffee.addEventListener('click', () => {
+    let cursor = '/img/coffee.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+
+  const mario = document.getElementById('button15');
+  mario.addEventListener('click', () => {
+    let cursor = '/img/mario.png';
+    document.body.style.cursor = `url(${cursor}), auto`;
+  });
+});
